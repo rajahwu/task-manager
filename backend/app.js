46ce0c99474c41068853fc5fc38dd2fe78edde26
rajahwu <vmradford@gmail.com>
2023-10-01 00:16:55 -0400

    connect to mysql development db

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,17 @@ const app = express();
 app.use(express.json());
 
 const port = 3000;
+const { User } = require('./db/models');
 
 app.get("/", (req, res) => {
   res.json({ title: "Home" });
 });
+
+app.get('/users', async (req, res) => {
+  // const users = await db.Users.findAll()
+  console.log(User)
+  res.json({users: "users"})
+})
 
 
 app.listen(port, () => {
